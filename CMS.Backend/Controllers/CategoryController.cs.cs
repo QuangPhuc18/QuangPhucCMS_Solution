35f51d2e225d1915d2bb4008caef1f5b1aa26c81
QuangPhuc18 <lequangphuc18092005@gmail.com>
2026-05-16 09:38:02 +0700

    Buoc4,5,renluyen

---
+++ CMS.Backend/Controllers/CategoryController.cs.cs
@@ -0,0 +1,26 @@
+﻿//SinhVien: Lê Quang Phúc
+//MSSV:2123110118
+//Lớp: CCQ2311D
+//Ngày : 16/05/2026
+//Mô tả : Controller quản lý danh mục sản phẩm
+
+using CMS_DATA.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Backend.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Tạo danh sách dữ liệu mẫu trực tiếp trong code
+            var list = new List<Category>
+            {
+            new Category { Id = 1, Name = "Tin Công Nghệ", Description = "Review Laptop, AI" },
+            new Category { Id = 2, Name = "Giáo Dục", Description = "Thông tin tuyển sinh" }
+
+            };
+            return View(list); // Gửi danh sách này sang giao diện
+        }
+    }
+}
---
+++ CMS.Backend/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿//SinhVien: Lê Quang Phúc
+//MSSV:2123110118
+//Lớp: CCQ2311D
+//Ngày : 16/05/2026
+//Mô tả : Controller Quản lý người dùng (User)
+
+using CMS_DATA.Entities; // Phải có dòng này để dùng lớp User
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Backend.Controllers
+{
+    public class UserController : Controller
+    {
+        // Hàm Index: Hiển thị danh sách thành viên quản trị
+        public IActionResult Index()
+        {
+            // 1. Tạo danh sách Người dùng giả (Mock Data)
+            var users = new List<User>
+            {
+                new User
+                {
+                    Id = 1,
+                    Username = "admin_Phuc",
+                    FullName = "Lê Quang Phúc",
+                    Role = "Administrator"
+                },
+                new User
+                {
+                    Id = 2,
+                    Username = "editor_01",
+                    FullName = "Trần Văn Biên Tập",
+                    Role = "Editor"
+                },
+                new User
+                {
+                    Id = 3,
+                    Username = "author_minh",
+                    FullName = "Lê Quang Minh",
+                    Role = "Author"
+                }
+            };
+
+            // 2. Trả về View kèm theo danh sách người dùng
+            return View(users);
+        }
+    }
+}
